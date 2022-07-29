efbf6d0998b90fb710b387cb2914ab53a1c08e81
vijay <vijayramreddy990@gmail.com>
2022-07-30 01:36:58 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import GamingImg from './assets/images/misc/gaming.svg';
+
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.textStyle}>GAMEON</Text>
+      </View>
+      <View style={styles.imageWrapper}>
+        <GamingImg style={styles.imageStyle} width={300} height={300} />
+      </View>
+      <TouchableOpacity style={styles.touchableButton}>
+        <Text style={styles.touchableText}>Let's Begin</Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color="#FFF" />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  textStyle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#20315f',
+  },
+  imageWrapper: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageStyle: {
+    transform: [
+      {
+        rotate: '-15deg',
+      },
+    ],
+  },
+  touchableButton: {
+    backgroundColor: '#AD40AF',
+    padding: 20,
+    width: '90%',
+    borderRadius: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  touchableText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#FFF',
+    fontFamily: 'Inter-Bold',
+  },
+});
+
+export default App;
